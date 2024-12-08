e08ba249d57bdd2de9b891fc549d5a3ffe7a1406
Samantha <balle137@umn.edu>
2024-12-08 15:54:50 -0600

    changed name -> id

--- core/py/game.py
+++ core/py/game.py
@@ -1,13 +1,13 @@
 class Game:
-	def __init__(self, team1_name, team2_name, week, day, season, start, end, venue_name):
-		self.team1_name = team1_name
-		self.team2_name = team2_name
+	def __init__(self, team1_id, team2_id, week, day, season, start, end, venue_id):
+		self.team1_id = team1_id
+		self.team2_id = team2_id
 		self.week = week
 		self.day = day
 		self.season = season
 		self.start = start
 		self.end = end
-		self.venue_name = venue_name
+		self.venue_id = venue_id
 		
 	def dump(self):
-		print(f"Team {self.team1_name} vs. Team {self.team2_name} ({self.start}-{self.end}) {self.week} {self.day}")
+		print(f"Team {self.team1_id} vs. Team {self.team2_id} ({self.start}-{self.end}) {self.week} {self.day}")
